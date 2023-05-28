1b04e745227bbb988655587707a32898fe66bee3
frostime <frostime@foxmail.com>
2023-05-28 21:20:22 +0800

    feat

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Plugin, Menu } from "siyuan";
+import { Plugin, Menu, isMobile } from "siyuan";
 import { svg } from "./const";
 import { request } from "./api";
 import "./index.scss";
@@ -9,31 +9,40 @@ const SIYUAN = window.siyuan;
 export default class ThemeChangePlugin extends Plugin {
 
     onload() {
-        this.addTopBar({
+        const topBarElement = this.addTopBar({
             icon: svg,
             title: "主题切换",
-            callback: () => {
-                this.showThemesMenu();
+            callback: (event: MouseEvent) => {
+                this.showThemesMenu(topBarElement.getBoundingClientRect());
             }
         });
     }
 
-    showThemesMenu() {
+    showThemesMenu(rect: DOMRect) {
         let menu: Menu = new Menu("ThemeChange");
         const appearance = SIYUAN.config.appearance;
         const mode = appearance.mode === 0 ? 'light' : 'dark';
         const themes: string[] = mode === 'light' ? appearance.lightThemes : appearance.darkThemes;
-        for (let theme of themes) {
+        for (const theme of themes) {
             menu.addItem({
                 label: theme,
-                click(theme) {
+                click: () => {
                     this.useTheme(theme, mode);
                 }
             });
         }
+        if (isMobile()) {
+            menu.fullscreen();
+        } else {
+            menu.open({
+                x: rect.right,
+                y: rect.bottom,
+                isLeft: true,
+            });
+        }
     }
 
-    useTheme(theme, mode) {
+    private useTheme(theme: string, mode: string) {
         const appearance = SIYUAN.config.appearance;
         const current = mode === 'light' ? appearance.themeLight : appearance.themeDark;
         if (theme === current) {
