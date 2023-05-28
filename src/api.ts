93afb026fe45a3c3f0114a2d2b6fcdbd7da511da
frostime <frostime@foxmail.com>
2023-05-28 21:10:04 +0800

    更改代码

--- src/api.ts
+++ src/api.ts
@@ -13,7 +13,7 @@ import {
 import { fetchSyncPost, IWebSocketData } from "siyuan";
 
 
-async function request(url: string, data: any) {
+export async function request(url: string, data: any) {
     let response: IWebSocketData = await fetchSyncPost(url, data);
     let res = response.code === 0 ? response.data : null;
     return res;
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,52 @@
-import { Plugin, showMessage, confirm, Dialog, Menu, isMobile, openTab, adaptHotkey } from "siyuan";
+import { Plugin, Menu } from "siyuan";
+import { svg } from "./const";
+import { request } from "./api";
 import "./index.scss";
-export default class SamplePlugin extends Plugin {
 
-    onload() [
-        
-    ]
+// @ts-ignore
+const SIYUAN = window.siyuan;
+
+export default class ThemeChangePlugin extends Plugin {
+
+    onload() {
+        this.addTopBar({
+            icon: svg,
+            title: "主题切换",
+            callback: () => {
+                this.showThemesMenu();
+            }
+        });
+    }
+
+    showThemesMenu() {
+        let menu: Menu = new Menu("ThemeChange");
+        const appearance = SIYUAN.config.appearance;
+        const mode = appearance.mode === 0 ? 'light' : 'dark';
+        const themes: string[] = mode === 'light' ? appearance.lightThemes : appearance.darkThemes;
+        for (let theme of themes) {
+            menu.addItem({
+                label: theme,
+                click(theme) {
+                    this.useTheme(theme, mode);
+                }
+            });
+        }
+    }
+
+    useTheme(theme, mode) {
+        const appearance = SIYUAN.config.appearance;
+        const current = mode === 'light' ? appearance.themeLight : appearance.themeDark;
+        if (theme === current) {
+            return;
+        }
+        const obj = {
+            ...SIYUAN.config.appearance,
+        };
+        if (mode === 'light') {
+            obj.themeLight = theme;
+        } else {
+            obj.themeDark = theme;
+        }
+        request('/api/setting/setAppearance', obj).then(() => window.location.reload());
+    }
 }
